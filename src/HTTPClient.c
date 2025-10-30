3e1887e6070264b07d50ffc839e415589ff1a4d5
TheFreeDuck <fredrikthorcewers@gmail.com>
2025-10-30 12:42:20 +0100

    Add HTTPClient that uses TCPClient

---
+++ src/HTTPClient.c
@@ -0,0 +1,85 @@
+#include "HTTPClient.h"
+#include "TCPClient.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void HTTPClient_init(HTTPClient *c, int FD) {
+  TCPClient tcp_client;
+  TCPClient_Initiate(&tcp_client, FD);
+  c->tcp_client = tcp_client;
+}
+
+int HTTPClient_initPtr(HTTPClient **c, int FD) {
+  HTTPClient *http_client = (HTTPClient *)malloc(sizeof(HTTPClient));
+  if (!http_client) {
+    return -1;
+  }
+
+  HTTPClient_init(http_client, FD);
+  *c = http_client;
+  return 0;
+}
+
+int HTTPClient_connect(HTTPClient *c, const char *host, const char *port) {
+  c->host = (char *)host;
+  return TCPClient_Connect(&c->tcp_client, host, port);
+}
+
+int HTTPClient_Write(HTTPClient *c, const char *endpoint, const char *method,
+                     const char *body) {
+  char *response_body = NULL;
+  if (!body) {
+    response_body = "";
+  } else {
+    response_body = (char *)body;
+  }
+
+  size_t endpoint_len = strlen(endpoint);
+  size_t method_len = strlen(method);
+  size_t body_len = strlen(response_body);
+
+  size_t request_len = endpoint_len + method_len + body_len + 120;
+
+  char *request = malloc(request_len);
+  if (!request) {
+    return -1;
+  }
+
+  int bytes_written =
+      snprintf(request, request_len,
+               "%s %s HTTP/1.1\r\nHost: %s\r\nContent-Length: "
+               "%zu\r\nConnection: close\r\n\r\n%s",
+               method, endpoint, c->host, body_len, response_body);
+
+  if (bytes_written < 0) {
+    free(request);
+    return -1;
+  }
+
+  int sent = TCPClient_WriteAll(&c->tcp_client, (const uint8_t *)request,
+                                bytes_written);
+  free(request);
+
+  return sent;
+}
+
+int HTTPClient_Read(HTTPClient *c, uint8_t *buf, int len,
+                    response_callback callback) {
+  int recived = TCPClient_ReadAll(&c->tcp_client, buf, len);
+  callback((char *)buf);
+  return recived;
+}
+
+void HTTPClient_Disconnect(HTTPClient *c) {
+  TCPClient_Disconnect(&c->tcp_client);
+}
+
+void HTTPClient_Dispose(HTTPClient *c) { TCPClient_Dispose(&c->tcp_client); }
+
+void HTTPClient_DisposePtr(HTTPClient *c) {
+  HTTPClient_Dispose(c);
+  free(c);
+}
---
+++ src/HTTPClient.h
@@ -0,0 +1,47 @@
+/// Simple TCP based HTTP client that writes and reads all.
+/// This is a blocking HTTPClient
+/// Written on top of TCPClient.h
+#ifndef __HTTPClient_h_
+#define __HTTPClient_h_
+
+#include "TCPClient.h"
+#include <netdb.h>
+#include <stdint.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+typedef void (*response_callback)(const char *response);
+
+typedef struct {
+  TCPClient tcp_client;
+  char *host;
+
+} HTTPClient;
+
+void HTTPClient_init(HTTPClient *c, int FD);
+
+// Create HTTPClient on heap
+// Make sure to use HTTPClient_DisposePtr to dispose it correctly
+int HTTPClient_initPtr(HTTPClient **c, int FD);
+
+int HTTPClient_connect(HTTPClient *c, const char *host, const char *port);
+
+/// Creats relevant headers from method and endpoint.
+/// Uses Connection: close header to intsantly close.
+/// Adds correct body length header
+int HTTPClient_Write(HTTPClient *c, const char *endpoint, const char *method,
+                     const char *body);
+
+// Reads all data that it can into the give buffer at once.
+// If the buffer is too small the message will be cut of.
+int HTTPClient_Read(HTTPClient *c, uint8_t *buf, int len,
+                    response_callback callback);
+
+void HTTPClient_Disconnect(HTTPClient *c);
+
+void HTTPClient_Dispose(HTTPClient *c);
+
+void HTTPClient_DisposePtr(HTTPClient *c);
+
+#endif // __HTTPClient_h_
